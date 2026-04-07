046fa153272956ef967d519bd33dc3b99aa48713
VGHC6 <2938476516@qq.com>
2026-04-07 11:48:39 +0800

    角色移动
    添加了角色移动的功能

---
+++ Project/Assets/scripes/playerMove.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Tilemaps;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 5;
+    public int facingDirection = 1;//1表示向右，-1表示向左
+    public Rigidbody2D rb;
+    public Animator animator;//动画控制器
+
+
+    // Update is called once per frame
+    void FixedUpdate()//以固定的帧率运行，适合物理相关的更新
+    {
+        float horizontal = Input.GetAxis("Horizontal");//监听左右方向
+        float vertical = Input.GetAxis("Vertical");//监听上下方向
+
+        //根据输入设置动画参数，Mathf.Abs()函数返回绝对值，确保动画参数为正数
+        animator.SetFloat("horizontal", Mathf.Abs(horizontal));//设置动画参数
+        animator.SetFloat("vertical", Mathf.Abs(vertical));
+
+        if (horizontal > 0 && transform.localScale.x < 0 || horizontal < 0 && transform.localScale.x > 0)
+        {
+            //如果向右移动且当前朝向为左
+            Flip();//反转角色
+        }
+        rb.velocity=new Vector2(horizontal, vertical) * speed;//设置速度
+
+    }
+
+    void Flip()
+    {
+        facingDirection *= -1;//改变朝向
+        transform.localScale=new Vector3(facingDirection * Mathf.Abs(transform.localScale.x), transform.localScale.y, transform.localScale.z);//反转角色的x轴缩放
+    }
+}
